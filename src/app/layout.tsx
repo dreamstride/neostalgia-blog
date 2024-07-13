265b081e1f4d2f06f854ac9ec4b7f15dadaccb83
dreamstride <regdshaner@gmail.com>
2024-07-12 23:32:56 -0400

    Add Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import localFont from 'next/font/local'
+import localFont from 'next/font/local';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 
@@ -27,6 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <SpeedInsights/>
       <body className={`${GeistMono.variable} ${inter.variable} ${migha.variable}`}>{children}</body>
     </html>
   );
